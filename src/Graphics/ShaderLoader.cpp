582b1118029de7afebd10d3e826a4e67158f574e
cepebel40-beep <cepebel40@gmail.com>
2026-07-30 23:55:28 +0700

    Update ShaderLoader.cpp

--- src/Graphics/ShaderLoader.cpp
+++ src/Graphics/ShaderLoader.cpp
@@ -1,68 +1,32 @@
 #include "Graphics/ShaderLoader.hpp"
 #include "Core/Logger.hpp"
 
-#include <fstream>
-#include <sstream>
-#include <string>
-
-#ifdef __ANDROID__
-#include <GLES3/gl3.h>
-#endif
-
 namespace PHX
 {
 
-static std::string ReadTextFile(const char* path)
-{
-    std::ifstream file(path);
-
-    if(!file.is_open())
-    {
-        Logger::Error("Failed to open shader file.");
-        return "";
-    }
-
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-
-    return buffer.str();
-}
-
-ShaderLoader::ShaderLoader()
-{
-    Logger::Info("ShaderLoader initialized");
-}
-
-ShaderLoader::~ShaderLoader()
-{
-    Logger::Info("ShaderLoader destroyed");
-}
-
 bool ShaderLoader::Load(const char* vertexPath, const char* fragmentPath)
 {
-    if(vertexPath == nullptr || fragmentPath == nullptr)
+    Logger::Info("================================");
+    Logger::Info("Phoenix ShaderLoader");
+    Logger::Info("Initializing...");
+    Logger::Info("================================");
+
+    if (vertexPath == nullptr)
     {
-        Logger::Error("Shader path invalid.");
+        Logger::Error("Vertex shader path is null.");
         return false;
     }
 
-    std::string vertexSource = ReadTextFile(vertexPath);
-    std::string fragmentSource = ReadTextFile(fragmentPath);
-
-    if(vertexSource.empty())
+    if (fragmentPath == nullptr)
     {
-        Logger::Error("Vertex shader empty.");
+        Logger::Error("Fragment shader path is null.");
         return false;
     }
 
-    if(fragmentSource.empty())
-    {
-        Logger::Error("Fragment shader empty.");
-        return false;
-    }
+    Logger::Info("Vertex shader path OK.");
+    Logger::Info("Fragment shader path OK.");
 
-    Logger::Info("Vertex shader loaded.");
-    Logger::Info("Fragment shader loaded.");
+    Logger::Info("ShaderLoader ready.");
 
     return true;
 }
