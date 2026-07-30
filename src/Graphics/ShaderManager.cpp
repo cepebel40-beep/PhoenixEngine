7931c6e60602b2e1ace825101772ed080af8ef71
cepebel40-beep <cepebel40@gmail.com>
2026-07-31 00:47:35 +0700

    Phoenix Alpha 0.2 - ShaderManager Foundation
    Added ShaderLoader initialization and improved logging.

--- src/Graphics/ShaderManager.cpp
+++ src/Graphics/ShaderManager.cpp
@@ -1,7 +1,20 @@
 #include "Graphics/ShaderManager.hpp"
+#include "Graphics/ShaderLoader.hpp"
 #include "Core/Logger.hpp"
-namespace PHX{
-bool ShaderManager::Initialize(){
- Logger::Info("ShaderManager initialized");
- return true;
-}}
+
+namespace PHX
+{
+
+bool ShaderManager::Initialize()
+{
+    Logger::Info("Initializing ShaderManager...");
+
+    ShaderLoader loader;
+
+    Logger::Info("ShaderLoader ready.");
+    Logger::Info("ShaderManager initialized.");
+
+    return true;
+}
+
+}
