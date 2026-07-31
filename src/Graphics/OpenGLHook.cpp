8778fb881d68ef08660d811e9560a89c86428703
cepebel40-beep <cepebel40@gmail.com>
2026-07-31 14:54:29 +0700

    Phoenix Alpha 3.7 - Add OpenGLHook

---
+++ src/Graphics/OpenGLHook.cpp
@@ -0,0 +1,24 @@
+#include "Graphics/OpenGLHook.hpp"
+
+namespace PHX
+{
+
+bool OpenGLHook::sInstalled = false;
+
+bool OpenGLHook::Install()
+{
+    sInstalled = true;
+    return true;
+}
+
+void OpenGLHook::Remove()
+{
+    sInstalled = false;
+}
+
+bool OpenGLHook::IsInstalled()
+{
+    return sInstalled;
+}
+
+}
