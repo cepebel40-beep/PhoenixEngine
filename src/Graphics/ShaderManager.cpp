51428c55b90e214210e35bbebbb86c45ede6e221
cepebel40-beep <cepebel40@gmail.com>
2026-07-31 12:54:05 +0700

    Refactor ShaderManager logging and cleanup
    Updated logging macros and removed unused shader initialization code.

--- src/Graphics/ShaderManager.cpp
+++ src/Graphics/ShaderManager.cpp
@@ -1,64 +1,24 @@
-#include "ShaderManager.hpp"
+#include "Graphics/ShaderManager.hpp"
+
 #include <GLES3/gl3.h>
 #include <android/log.h>
 
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,"Phoenix",__VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"Phoenix",__VA_ARGS__)
+#define LOG_TAG "Phoenix64"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-static GLuint gVertexShader = 0;
-static GLuint gFragmentShader = 0;
-static GLuint gProgram = 0;
-
-static const char* DefaultVertexShader =
-"#version 300 es\n"
-"layout(location=0) in vec3 aPos;\n"
-"void main(){\n"
-"gl_Position=vec4(aPos,1.0);\n"
-"}";
-
-static const char* DefaultFragmentShader =
-"#version 300 es\n"
-"precision mediump float;\n"
-"out vec4 FragColor;\n"
-"void main(){\n"
-"FragColor=vec4(1.0,1.0,1.0,1.0);\n"
-"}";
+namespace Phoenix
+{
 
 bool ShaderManager::Initialize()
 {
-    gVertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(gVertexShader,1,&DefaultVertexShader,nullptr);
-    glCompileShader(gVertexShader);
-
-    GLint success = 0;
-    glGetShaderiv(gVertexShader,GL_COMPILE_STATUS,&success);
-
-    if(!success)
-    {
-        LOGE("Vertex Shader Compile Failed");
-        return false;
-    }
-
-    LOGI("Vertex Shader OK");
-
-    gFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(gFragmentShader,1,&DefaultFragmentShader,nullptr);
-    glCompileShader(gFragmentShader);
-
-    glGetShaderiv(gFragmentShader,GL_COMPILE_STATUS,&success);
-
-    if(!success)
-    {
-        LOGE("Fragment Shader Compile Failed");
-        return false;
-    }
-
-    LOGI("Fragment Shader OK");
-
-    gProgram = glCreateProgram();
-    glAttachShader(gProgram,gVertexShader);
-    glAttachShader(gProgram,gFragmentShader);
-    glLinkProgram(gProgram);
-
+    LOGI("ShaderManager initialized");
     return true;
+}
+
+void ShaderManager::Shutdown()
+{
+    LOGI("ShaderManager shutdown");
+}
+
 }
