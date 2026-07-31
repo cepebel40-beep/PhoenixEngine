ca84a2f30e8bad5c0e9f9d639cd27f10b6a789d8
cepebel40-beep <cepebel40@gmail.com>
2026-07-31 12:58:51 +0700

    Refactor ShaderManager and add default shaders

--- src/Graphics/ShaderManager.cpp
+++ src/Graphics/ShaderManager.cpp
@@ -3,22 +3,73 @@
 #include <GLES3/gl3.h>
 #include <android/log.h>
 
-#define LOG_TAG "Phoenix64"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,"Phoenix",__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"Phoenix",__VA_ARGS__)
 
-namespace Phoenix
+namespace PHX
 {
+
+static const char* DefaultVertexShader =
+"#version 300 es\n"
+"layout(location=0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos,1.0);\n"
+"}";
+
+static const char* DefaultFragmentShader =
+"#version 300 es\n"
+"precision mediump float;\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(1.0,1.0,1.0,1.0);\n"
+"}";
 
 bool ShaderManager::Initialize()
 {
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs,1,&DefaultVertexShader,nullptr);
+    glCompileShader(vs);
+
+    GLint success = GL_FALSE;
+    glGetShaderiv(vs,GL_COMPILE_STATUS,&success);
+
+    if(!success)
+    {
+        LOGE("Vertex shader compile failed");
+        return false;
+    }
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs,1,&DefaultFragmentShader,nullptr);
+    glCompileShader(fs);
+
+    glGetShaderiv(fs,GL_COMPILE_STATUS,&success);
+
+    if(!success)
+    {
+        LOGE("Fragment shader compile failed");
+        glDeleteShader(vs);
+        return false;
+    }
+
+    mProgram = glCreateProgram();
+    glAttachShader(mProgram,vs);
+    glAttachShader(mProgram,fs);
+    glLinkProgram(mProgram);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
     LOGI("ShaderManager initialized");
+
     return true;
 }
 
-void ShaderManager::Shutdown()
+GLuint ShaderManager::GetProgram() const
 {
-    LOGI("ShaderManager shutdown");
+    return mProgram;
 }
 
 }
