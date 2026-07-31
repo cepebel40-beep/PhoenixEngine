4fafd67180d7bda69da6742d88937225920f4fad
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 01:29:10 +0700

    Update EngineState.hpp

--- include/System/EngineState.hpp
+++ include/System/EngineState.hpp
@@ -1,1 +1,16 @@
+#pragma once
 
+namespace PHX
+{
+
+class EngineState
+{
+public:
+    static bool IsInitialized();
+    static void SetInitialized(bool initialized);
+
+private:
+    static bool sInitialized;
+};
+
+}
