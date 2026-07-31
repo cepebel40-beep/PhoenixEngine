d5f645b72532bd98568fe8f233b2bf6fdd5f3393
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 01:21:24 +0700

    Implement StartupSequence

--- src/System/StartupSequence.cpp
+++ src/System/StartupSequence.cpp
@@ -1,1 +1,24 @@
+#include "System/StartupSequence.hpp"
 
+#include "Core/Engine.hpp"
+#include "Core/Logger.hpp"
+
+namespace PHX
+{
+
+bool StartupSequence::Run()
+{
+    Logger::Info("Starting Phoenix...");
+
+    if (!InitializeEngine())
+    {
+        Logger::Error("Engine startup failed.");
+        return false;
+    }
+
+    Logger::Info("Phoenix started.");
+
+    return true;
+}
+
+}
