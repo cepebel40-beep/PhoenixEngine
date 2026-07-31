5f242a2618924be2858685592555cfc666ce164e
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 01:40:11 +0700

    Update RuntimeMonitor.cpp

--- src/System/RuntimeMonitor.cpp
+++ src/System/RuntimeMonitor.cpp
@@ -1,1 +1,18 @@
+#include "System/RuntimeMonitor.hpp"
 
+namespace PHX
+{
+
+bool RuntimeMonitor::sRunning = false;
+
+void RuntimeMonitor::Update()
+{
+    sRunning = true;
+}
+
+bool RuntimeMonitor::IsRunning()
+{
+    return sRunning;
+}
+
+}
