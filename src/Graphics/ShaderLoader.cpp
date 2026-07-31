9e2a07c6e78bf3e0894a1ac52d570632871c4c97
cepebel40-beep <cepebel40@gmail.com>
2026-07-31 10:46:59 +0700

    Phoenix Alpha 0.6 - ShaderLoader Validation
    Removed the Load function and simplified LoadDefaultShaders method to directly log success for common.glsl.

--- src/Graphics/ShaderLoader.cpp
+++ src/Graphics/ShaderLoader.cpp
@@ -1,67 +1,20 @@
 #include "Graphics/ShaderLoader.hpp"
 #include "Core/Logger.hpp"
 
-#include <fstream>
-#include <sstream>
-#include <string>
-
 namespace PHX
 {
 
-static bool ReadTextFile(const char* path, std::string& output)
+bool ShaderLoader::LoadDefaultShaders()
 {
-    std::ifstream file(path);
+    std::string shaderSource;
 
-    if (!file.is_open())
+    if (!ReadTextFile("gta/files/phoenix/shaders/include/common.glsl", shaderSource))
     {
-        Logger::Error(path);
+        Logger::Error("Failed to load common.glsl");
         return false;
     }
 
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    output = buffer.str();
-
-    return true;
-}
-
-bool ShaderLoader::Load(const char* vertexPath, const char* fragmentPath)
-{
-    Logger::Info("================================");
-    Logger::Info("Phoenix ShaderLoader");
-    Logger::Info("Initializing...");
-    Logger::Info("================================");
-
-    if (vertexPath == nullptr)
-    {
-        Logger::Error("Vertex shader path is null.");
-        return false;
-    }
-
-    if (fragmentPath == nullptr)
-    {
-        Logger::Error("Fragment shader path is null.");
-        return false;
-    }
-
-    std::string vertexSource;
-    std::string fragmentSource;
-
-    if (!ReadTextFile(vertexPath, vertexSource))
-    {
-        Logger::Error("Failed to read vertex shader.");
-        return false;
-    }
-
-    if (!ReadTextFile(fragmentPath, fragmentSource))
-    {
-        Logger::Error("Failed to read fragment shader.");
-        return false;
-    }
-
-    Logger::Info("Vertex shader loaded.");
-    Logger::Info("Fragment shader loaded.");
-    Logger::Info("ShaderLoader ready.");
+    Logger::Info("common.glsl loaded successfully");
 
     return true;
 }
