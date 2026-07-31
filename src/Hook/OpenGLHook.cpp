bb1d2514f8ae0256e986672dd4b95bc6acca5c93
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 00:39:42 +0700

    Update OpenGLHook.cpp

--- src/Hook/OpenGLHook.cpp
+++ src/Hook/OpenGLHook.cpp
@@ -1,2 +1,25 @@
+#include "Hook/OpenGLHook.hpp"
+
+#include "Graphics/OpenGLHook.hpp"
+
 #include "Core/Logger.hpp"
-namespace PHX{bool InstallOpenGLHooks(){Logger::Info("OpenGL hook placeholder");return true;}}
+
+namespace PHX
+{
+
+bool InstallOpenGLHooks()
+{
+    Logger::Info("Installing OpenGL hooks...");
+
+    if (!OpenGLHook::Install())
+    {
+        Logger::Error("OpenGLHook::Install() failed.");
+        return false;
+    }
+
+    Logger::Info("OpenGL hooks installed.");
+
+    return true;
+}
+
+}
