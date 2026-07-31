446f88480e114bba08f9a2bd60638060ba3457bd
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 02:55:41 +0700

    Update OpenGLHook.cpp

--- src/Hook/OpenGLHook.cpp
+++ src/Hook/OpenGLHook.cpp
@@ -1,15 +1,47 @@
 #include "Hook/OpenGLHook.hpp"
 
+#include <GLES3/gl3.h>
+#include <android/log.h>
+
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "Phoenix", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "Phoenix", __VA_ARGS__)
+
 namespace PHX
 {
 
+static bool gHooksInstalled = false;
+
 bool InstallOpenGLHooks()
 {
+    if (gHooksInstalled)
+    {
+        LOGI("OpenGL hooks already installed");
+        return true;
+    }
+
+    LOGI("Installing OpenGL hooks...");
+
+    // Placeholder.
+    // Hook eglSwapBuffers, glShaderSource,
+    // glUseProgram, dll akan dipasang di sini.
+
+    gHooksInstalled = true;
+
+    LOGI("OpenGL hooks installed");
+
     return true;
 }
 
 void RemoveOpenGLHooks()
 {
+    if (!gHooksInstalled)
+        return;
+
+    LOGI("Removing OpenGL hooks...");
+
+    gHooksInstalled = false;
+
+    LOGI("OpenGL hooks removed");
 }
 
 }
