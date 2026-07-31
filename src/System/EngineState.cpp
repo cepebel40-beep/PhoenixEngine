66b0a81e2ad2d57f32560ea6297e2758360dd801
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 01:29:43 +0700

    Implement EngineState initialization methods

--- src/System/EngineState.cpp
+++ src/System/EngineState.cpp
@@ -1,1 +1,18 @@
+#include "System/EngineState.hpp"
 
+namespace PHX
+{
+
+bool EngineState::sInitialized = false;
+
+bool EngineState::IsInitialized()
+{
+    return sInitialized;
+}
+
+void EngineState::SetInitialized(bool initialized)
+{
+    sInitialized = initialized;
+}
+
+}
