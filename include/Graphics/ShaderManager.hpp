194c923c34c323dfbc0928dc21deee0798cd941f
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 02:15:01 +0700

    Create ShaderManager interface
    Added constructor and new shader loading methods.

--- include/Graphics/ShaderManager.hpp
+++ include/Graphics/ShaderManager.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <GLES3/gl3.h>
+#include <string>
 
 namespace PHX
 {
@@ -8,12 +9,20 @@ namespace PHX
 class ShaderManager
 {
 public:
+    ShaderManager();
+
     bool Initialize();
 
-    GLuint GetProgram() const;
+    GLuint LoadShader(GLenum type,
+                      const std::string& source);
+
+    GLuint CreateProgram(const std::string& vertexSource,
+                         const std::string& fragmentSource);
+
+    void Destroy();
 
 private:
-    GLuint mProgram = 0;
+    GLuint mProgram;
 };
 
 }
