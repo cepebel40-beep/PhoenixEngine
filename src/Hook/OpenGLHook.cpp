d89c1671157495bbd4e2c1a67327c24fb7200122
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 01:14:19 +0700

    Restore OpenGLHook stub implementation
    Removed OpenGL runtime initialization and function table loading from InstallOpenGLHooks.

--- src/Hook/OpenGLHook.cpp
+++ src/Hook/OpenGLHook.cpp
@@ -1,25 +1,15 @@
 #include "Hook/OpenGLHook.hpp"
 
-#include "Graphics/OpenGLRuntime.hpp"
-#include "Graphics/OpenGLFunctionTable.hpp"
-
 namespace PHX
 {
 
 bool InstallOpenGLHooks()
 {
-    if (!InitializeOpenGLRuntime())
-        return false;
-
-    if (!LoadOpenGLFunctionTable())
-        return false;
-
     return true;
 }
 
 void RemoveOpenGLHooks()
 {
-    // Tahap berikutnya akan menghapus hook asli di sini.
 }
 
 }
