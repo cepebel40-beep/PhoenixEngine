6828f8ebb9003171f771b5c599c058569d66714d
cepebel40-beep <cepebel40@gmail.com>
2026-07-31 13:46:57 +0700

    Add ShaderCompiler class for shader compilation

---
+++ include/Graphics/ShaderCompiler.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <GLES3/gl3.h>
+
+namespace PHX
+{
+
+class ShaderCompiler
+{
+public:
+    static GLuint CompileVertex(const std::string& source);
+    static GLuint CompileFragment(const std::string& source);
+
+private:
+    static GLuint Compile(GLenum type, const std::string& source);
+};
+
+}
