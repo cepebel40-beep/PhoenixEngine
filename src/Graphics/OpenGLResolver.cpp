edbbb62c07aa397955ec5c6ab76b8e41ee337f1f
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 03:30:00 +0700

    Improve OpenGLResolver logging
    Added logging for OpenGL library loading status.

--- src/Graphics/OpenGLResolver.cpp
+++ src/Graphics/OpenGLResolver.cpp
@@ -1,26 +1,40 @@
 #include "Graphics/OpenGLResolver.hpp"
 
+#include <android/log.h>
 #include <dlfcn.h>
+
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "Phoenix", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "Phoenix", __VA_ARGS__)
 
 namespace PHX
 {
-    static void* gGLESHandle = nullptr;
 
-    bool OpenGLResolver::Initialize()
+static void* gGLESHandle = nullptr;
+
+bool OpenGLResolver::Initialize()
+{
+    if (gGLESHandle)
+        return true;
+
+    gGLESHandle = dlopen("libGLESv2.so", RTLD_NOW);
+
+    if (!gGLESHandle)
     {
-        if (gGLESHandle)
-            return true;
-
-        gGLESHandle = dlopen("libGLESv2.so", RTLD_NOW);
-
-        return gGLESHandle != nullptr;
+        LOGE("Failed to load libGLESv2.so");
+        return false;
     }
 
-    void* OpenGLResolver::GetProcAddress(const char* name)
-    {
-        if (!gGLESHandle)
-            return nullptr;
+    LOGI("libGLESv2.so loaded");
 
-        return dlsym(gGLESHandle, name);
-    }
+    return true;
+}
+
+void* OpenGLResolver::GetProcAddress(const char* name)
+{
+    if (!gGLESHandle)
+        return nullptr;
+
+    return dlsym(gGLESHandle, name);
+}
+
 }
