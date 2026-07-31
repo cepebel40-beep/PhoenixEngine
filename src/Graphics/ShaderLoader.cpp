20f36a4c0ffe327daa2f86d17f7e22b1fd02236a
cepebel40-beep <cepebel40@gmail.com>
2026-07-31 11:00:34 +0700

    Update ShaderLoader.cpp

--- src/Graphics/ShaderLoader.cpp
+++ src/Graphics/ShaderLoader.cpp
@@ -1,6 +1,10 @@
 #include "Graphics/ShaderLoader.hpp"
 #include "Core/Logger.hpp"
 
+#include <string>
+#include <fstream>
+#include <sstream>
+
 namespace PHX
 {
 
@@ -8,13 +12,29 @@ bool ShaderLoader::LoadDefaultShaders()
 {
     std::string shaderSource;
 
-    if (!ReadTextFile("gta/files/phoenix/shaders/include/common.glsl", shaderSource))
+    if (!ReadTextFile(
+            "gta/files/phoenix/shaders/include/common.glsl",
+            shaderSource))
     {
         Logger::Error("Failed to load common.glsl");
         return false;
     }
 
     Logger::Info("common.glsl loaded successfully");
+    return true;
+}
+
+bool ShaderLoader::ReadTextFile(const char* path, std::string& buffer)
+{
+    std::ifstream file(path);
+
+    if (!file.is_open())
+        return false;
+
+    std::stringstream stream;
+    stream << file.rdbuf();
+
+    buffer = stream.str();
 
     return true;
 }
