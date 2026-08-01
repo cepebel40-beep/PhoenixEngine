94e602c655d3385524219ed35c2fc793794a890d
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 21:22:31 +0700

    Fix ShaderManager implementation
    Refactor shader source handling and logging.

--- src/Graphics/ShaderManager.cpp
+++ src/Graphics/ShaderManager.cpp
@@ -3,8 +3,8 @@
 #include <GLES3/gl3.h>
 #include <android/log.h>
 
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "Phoenix", __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "Phoenix", __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,"Phoenix",__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"Phoenix",__VA_ARGS__)
 
 namespace PHX
 {
@@ -19,74 +19,59 @@ ShaderManager::~ShaderManager()
     Destroy();
 }
 
-static const char* DefaultVertexShader =
-"#version 300 es\n"
-"layout(location=0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos,1.0);\n"
-"}";
-
-static const char* DefaultFragmentShader =
-"#version 300 es\n"
-"precision mediump float;\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(1.0,1.0,1.0,1.0);\n"
-"}";
-
 bool ShaderManager::Initialize()
 {
-    GLint success = GL_FALSE;
+    Destroy();
+
+    const char* vsSource =
+        "#version 300 es\n"
+        "layout(location=0) in vec3 aPos;\n"
+        "void main(){\n"
+        "gl_Position=vec4(aPos,1.0);\n"
+        "}";
+
+    const char* fsSource =
+        "#version 300 es\n"
+        "precision mediump float;\n"
+        "out vec4 FragColor;\n"
+        "void main(){\n"
+        "FragColor=vec4(1.0,1.0,1.0,1.0);\n"
+        "}";
 
     GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &DefaultVertexShader, nullptr);
+    glShaderSource(vs,1,&vsSource,nullptr);
     glCompileShader(vs);
 
-    glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        LOGE("Vertex shader compile failed");
-        glDeleteShader(vs);
-        return false;
-    }
-
     GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &DefaultFragmentShader, nullptr);
+    glShaderSource(fs,1,&fsSource,nullptr);
     glCompileShader(fs);
 
-    glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        glDeleteShader(vs);
-        glDeleteShader(fs);
-        return false;
-    }
-
     mProgram = glCreateProgram();
-
-    glAttachShader(mProgram, vs);
-    glAttachShader(mProgram, fs);
-
+    glAttachShader(mProgram,vs);
+    glAttachShader(mProgram,fs);
     glLinkProgram(mProgram);
-
-    glGetProgramiv(mProgram, GL_LINK_STATUS, &success);
 
     glDeleteShader(vs);
     glDeleteShader(fs);
 
-    return success == GL_TRUE;
+    if (mProgram == 0)
+    {
+        LOGE("Failed creating shader program");
+        return false;
+    }
+
+    LOGI("ShaderManager initialized");
+    return true;
 }
 
-GLuint ShaderManager::LoadShader(GLenum, const std::string&)
+GLuint ShaderManager::LoadShader(GLenum,
+                                 const std::string&)
 {
     return 0;
 }
 
-GLuint ShaderManager::CreateProgram(const std::string&, const std::string&)
+GLuint ShaderManager::CreateProgram(const std::string&,
+                                    const std::string&)
 {
     return mProgram;
 }
