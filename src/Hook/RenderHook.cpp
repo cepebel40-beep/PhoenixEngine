8d087cf217b0867cec02d41ba3b18f7cd0537ab3
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 20:58:57 +0700

    Implement InstallRenderHooks function with OpenGL setup

--- src/Hook/RenderHook.cpp
+++ src/Hook/RenderHook.cpp
@@ -1,6 +1,38 @@
+#include "Hook/RenderHook.hpp"
 #include "Core/Logger.hpp"
-namespace PHX{
-bool InstallRenderHooks(){
- Logger::Info("Render hooks placeholder");
- return true;
-}}
+
+#include <GLES3/gl3.h>
+
+namespace PHX
+{
+
+static bool gRendererInstalled = false;
+
+bool InstallRenderHooks()
+{
+    if (gRendererInstalled)
+    {
+        Logger::Info("Render hooks already installed");
+        return true;
+    }
+
+    Logger::Info("Installing Render hooks");
+
+    glDisable(GL_DITHER);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glFrontFace(GL_CCW);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    gRendererInstalled = true;
+
+    Logger::Info("Render hooks installed");
+
+    return true;
+}
+
+}
