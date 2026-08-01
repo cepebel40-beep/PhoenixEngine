2666dbe53a70d8a04627af0fbba0344357944c31
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 22:18:22 +0700

    Refactor HookManager class with additional methods

--- include/Hook/HookManager.hpp
+++ include/Hook/HookManager.hpp
@@ -4,16 +4,30 @@
 
 namespace PHX
 {
-    class HookManager
-    {
-    public:
 
-        static bool Initialize();
+class HookManager
+{
+public:
 
-        static bool Install(uintptr_t target,
-                            uintptr_t detour,
-                            uintptr_t* original);
+    static bool Initialize();
 
-        static bool Remove(uintptr_t target);
-    };
+    static bool Shutdown();
+
+    static bool InstallOpenGLHooks();
+
+    static bool RemoveOpenGLHooks();
+
+    static bool Install(uintptr_t target,
+                        uintptr_t detour,
+                        uintptr_t* original);
+
+    static bool Remove(uintptr_t target);
+
+    static bool IsInitialized();
+
+private:
+
+    static bool sInitialized;
+};
+
 }
