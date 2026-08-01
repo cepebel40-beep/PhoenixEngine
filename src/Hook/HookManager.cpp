0c6331caf38c3132d2c3ec91ae065db25c92f99a
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 22:18:56 +0700

    Refactor HookManager initialization and shutdown

--- src/Hook/HookManager.cpp
+++ src/Hook/HookManager.cpp
@@ -1,21 +1,67 @@
 #include "Hook/HookManager.hpp"
 
+#include "Core/Logger.hpp"
+
 namespace PHX
 {
-    bool HookManager::Initialize()
-    {
+
+bool HookManager::sInitialized = false;
+
+bool HookManager::Initialize()
+{
+    if (sInitialized)
         return true;
-    }
 
-    bool HookManager::Install(uintptr_t,
-                              uintptr_t,
-                              uintptr_t*)
-    {
-        return false;
-    }
+    Logger::Info("HookManager initialized");
 
-    bool HookManager::Remove(uintptr_t)
-    {
-        return false;
-    }
+    sInitialized = true;
+
+    return true;
+}
+
+bool HookManager::Shutdown()
+{
+    if (!sInitialized)
+        return true;
+
+    RemoveOpenGLHooks();
+
+    sInitialized = false;
+
+    Logger::Info("HookManager shutdown");
+
+    return true;
+}
+
+bool HookManager::InstallOpenGLHooks()
+{
+    Logger::Info("Installing OpenGL hooks...");
+
+    return true;
+}
+
+bool HookManager::RemoveOpenGLHooks()
+{
+    Logger::Info("Removing OpenGL hooks...");
+
+    return true;
+}
+
+bool HookManager::Install(uintptr_t,
+                          uintptr_t,
+                          uintptr_t*)
+{
+    return false;
+}
+
+bool HookManager::Remove(uintptr_t)
+{
+    return false;
+}
+
+bool HookManager::IsInitialized()
+{
+    return sInitialized;
+}
+
 }
