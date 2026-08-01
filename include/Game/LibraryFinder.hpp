4634f1ada2f16072ffbe27ecf248ec282af89b6d
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 20:14:06 +0700

    Update LibraryFinder.hpp

--- include/Game/LibraryFinder.hpp
+++ include/Game/LibraryFinder.hpp
@@ -7,11 +7,6 @@ namespace PHX
     class LibraryFinder
     {
     public:
-
-        static bool Initialize();
-
         static uintptr_t Find(const char* libraryName);
-
-        static bool IsLoaded(const char* libraryName);
     };
 }
