374a0e90d2b7d078231e282a772c5fdfa30b0472
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 21:42:41 +0700

    Improve ShaderPipeline implementation

--- src/Graphics/ShaderPipeline.cpp
+++ src/Graphics/ShaderPipeline.cpp
@@ -1,37 +1,85 @@
 #include "Graphics/ShaderPipeline.hpp"
-#include "Graphics/ShaderManager.hpp"
-#include "Core/Logger.hpp"
+#include "Graphics/ShaderCompiler.hpp"
+#include "Graphics/ShaderCache.hpp"
+
+#include <GLES3/gl3.h>
+#include <android/log.h>
+
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,"Phoenix",__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"Phoenix",__VA_ARGS__)
 
 namespace PHX
 {
 
-static ShaderManager gShaderManager;
-
-bool ShaderPipeline::Initialize()
+GLuint ShaderPipeline::Create(const std::string& name,
+                              const std::string& vertexSource,
+                              const std::string& fragmentSource)
 {
-    Logger::Info("Initializing Shader Pipeline...");
+    if (ShaderCache::Has(name))
+        return ShaderCache::Get(name);
 
-    if (!gShaderManager.Initialize())
+    GLuint vs = ShaderCompiler::CompileVertex(vertexSource);
+
+    if (!vs)
+        return 0;
+
+    GLuint fs = ShaderCompiler::CompileFragment(fragmentSource);
+
+    if (!fs)
     {
-        Logger::Error("Failed to initialize Shader Pipeline");
-        return false;
+        glDeleteShader(vs);
+        return 0;
     }
 
-    Logger::Info("Shader Pipeline initialized");
+    GLuint program = glCreateProgram();
 
-    return true;
+    glAttachShader(program,vs);
+    glAttachShader(program,fs);
+
+    glLinkProgram(program);
+
+    GLint success = GL_FALSE;
+
+    glGetProgramiv(program,
+                   GL_LINK_STATUS,
+                   &success);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    if (!success)
+    {
+        char log[1024]{};
+
+        glGetProgramInfoLog(program,
+                            sizeof(log),
+                            nullptr,
+                            log);
+
+        LOGE("%s",log);
+
+        glDeleteProgram(program);
+
+        return 0;
+    }
+
+    ShaderCache::Store(name,program);
+
+    LOGI("Shader pipeline created");
+
+    return program;
 }
 
-void ShaderPipeline::Update()
+void ShaderPipeline::Use(GLuint program)
 {
-    // Runtime update (reserved)
+    if(program!=0)
+        glUseProgram(program);
 }
 
-void ShaderPipeline::Shutdown()
+void ShaderPipeline::Destroy(GLuint program)
 {
-    gShaderManager.Destroy();
-
-    Logger::Info("Shader Pipeline destroyed");
+    if(program!=0)
+        glDeleteProgram(program);
 }
 
 }
