fcb8b1cf975ce5bed8fcd30238f3282d14eda9a0
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 22:45:44 +0700

    Implement OpenGLFunctionTable symbol resolution
    Initialized OpenGL function pointers using SymbolResolver.

--- src/Graphics/OpenGLFunctionTable.cpp
+++ src/Graphics/OpenGLFunctionTable.cpp
@@ -1,15 +1,49 @@
 #include "Graphics/OpenGLFunctionTable.hpp"
+#include "Memory/SymbolResolver.hpp"
 
 namespace PHX
 {
 
-PFNGLUSEPROGRAMPROC OpenGLFunctionTable::glUseProgramPtr = nullptr;
-PFNGLLINKPROGRAMPROC OpenGLFunctionTable::glLinkProgramPtr = nullptr;
+PFNGLUSEPROGRAMPROC    OpenGLFunctionTable::glUseProgramPtr    = nullptr;
+PFNGLLINKPROGRAMPROC   OpenGLFunctionTable::glLinkProgramPtr   = nullptr;
 PFNGLCOMPILESHADERPROC OpenGLFunctionTable::glCompileShaderPtr = nullptr;
+PFNGLCREATESHADERPROC  OpenGLFunctionTable::glCreateShaderPtr  = nullptr;
+PFNGLCREATEPROGRAMPROC OpenGLFunctionTable::glCreateProgramPtr = nullptr;
+PFNGLSHADERSOURCEPROC  OpenGLFunctionTable::glShaderSourcePtr  = nullptr;
 
 bool OpenGLFunctionTable::Initialize()
 {
-    return true;
+    glUseProgramPtr =
+        reinterpret_cast<PFNGLUSEPROGRAMPROC>(
+            SymbolResolver::ResolveOpenGL("glUseProgram"));
+
+    glLinkProgramPtr =
+        reinterpret_cast<PFNGLLINKPROGRAMPROC>(
+            SymbolResolver::ResolveOpenGL("glLinkProgram"));
+
+    glCompileShaderPtr =
+        reinterpret_cast<PFNGLCOMPILESHADERPROC>(
+            SymbolResolver::ResolveOpenGL("glCompileShader"));
+
+    glCreateShaderPtr =
+        reinterpret_cast<PFNGLCREATESHADERPROC>(
+            SymbolResolver::ResolveOpenGL("glCreateShader"));
+
+    glCreateProgramPtr =
+        reinterpret_cast<PFNGLCREATEPROGRAMPROC>(
+            SymbolResolver::ResolveOpenGL("glCreateProgram"));
+
+    glShaderSourcePtr =
+        reinterpret_cast<PFNGLSHADERSOURCEPROC>(
+            SymbolResolver::ResolveOpenGL("glShaderSource"));
+
+    return
+        glUseProgramPtr &&
+        glLinkProgramPtr &&
+        glCompileShaderPtr &&
+        glCreateShaderPtr &&
+        glCreateProgramPtr &&
+        glShaderSourcePtr;
 }
 
 void OpenGLFunctionTable::Shutdown()
@@ -17,6 +51,9 @@ void OpenGLFunctionTable::Shutdown()
     glUseProgramPtr = nullptr;
     glLinkProgramPtr = nullptr;
     glCompileShaderPtr = nullptr;
+    glCreateShaderPtr = nullptr;
+    glCreateProgramPtr = nullptr;
+    glShaderSourcePtr = nullptr;
 }
 
 }
