e502926a31d2175f941f20429b6a7775590a07fa
cepebel40-beep <cepebel40@gmail.com>
2026-08-01 23:32:58 +0700

    Refactor OpenGL hooks into OpenGLHook class

--- include/Hook/OpenGLHook.hpp
+++ include/Hook/OpenGLHook.hpp
@@ -1,10 +1,25 @@
 #pragma once
 
+#include <GLES3/gl3.h>
+
 namespace PHX
 {
 
-bool InstallOpenGLHooks();
-void RemoveOpenGLHooks();
-bool IsOpenGLHookInstalled();
+class OpenGLHook
+{
+public:
+
+    static bool Initialize();
+
+    static void Shutdown();
+
+private:
+
+    static void HookUseProgram();
+
+    static void HookCompileShader();
+
+    static void HookLinkProgram();
+};
 
 }
