6a29d2eb6aef30c27aee5a720fbd92315f067e65
cepebel40-beep <cepebel40@gmail.com>
2026-08-02 21:28:13 +0700

    Expand InstructionDecoder interface

--- include/Hook/InstructionDecoder.hpp
+++ include/Hook/InstructionDecoder.hpp
@@ -4,16 +4,26 @@
 
 namespace PHX
 {
-    class InstructionDecoder
-    {
-    public:
 
-        static uint32_t Read(uintptr_t address);
+class InstructionDecoder
+{
+public:
 
-        static bool IsBranch(uint32_t instruction);
+    static uint32_t Read(uintptr_t address);
 
-        static bool IsLoadLiteral(uint32_t instruction);
+    static bool IsBranch(uint32_t instruction);
 
-        static bool IsPcRelative(uint32_t instruction);
-    };
+    static bool IsLoadLiteral(uint32_t instruction);
+
+    static bool IsPcRelative(uint32_t instruction);
+
+    static bool IsBranchLink(uint32_t instruction);
+
+    static bool IsADR(uint32_t instruction);
+
+    static bool IsADRP(uint32_t instruction);
+
+    static bool IsB(uint32_t instruction);
+};
+
 }
