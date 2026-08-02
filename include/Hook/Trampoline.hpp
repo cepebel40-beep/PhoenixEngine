1e0dd6a7bf0c21d186a3ce294d2ab3888e2c19ff
cepebel40-beep <cepebel40@gmail.com>
2026-08-02 21:33:39 +0700

    Create Trampoline interface
    Added new methods for creating and managing trampoline instructions.

--- include/Hook/Trampoline.hpp
+++ include/Hook/Trampoline.hpp
@@ -5,14 +5,29 @@
 
 namespace PHX
 {
-    class Trampoline
-    {
-    public:
 
-        static void* Allocate(size_t size);
+class Trampoline
+{
+public:
 
-        static void Free(void* address);
+    static constexpr size_t kMaxInstructions = 8;
 
-        static uintptr_t GetAddress(void* trampoline);
-    };
+    static bool Create(void* target,
+                       void* replacement,
+                       void** trampoline);
+
+private:
+
+    static bool CopyInstructions(uint32_t* destination,
+                                 const uint32_t* source,
+                                 size_t count);
+
+    static bool RelocateInstruction(uint32_t* destination,
+                                    uintptr_t sourcePc,
+                                    uintptr_t destinationPc);
+
+    static void WriteAbsoluteJump(uint32_t* destination,
+                                  uintptr_t address);
+};
+
 }
