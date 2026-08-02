8c6da1702e34c6d86f2bb1b6d0d40f478367a5a0
cepebel40-beep <cepebel40@gmail.com>
2026-08-02 21:59:48 +0700

    Implement ADR and ADRP detection
    Refactor InstructionDecoder methods to return false or 0.

--- src/Hook/InstructionDecoder.cpp
+++ src/Hook/InstructionDecoder.cpp
@@ -3,46 +3,44 @@
 namespace PHX
 {
 
-uint32_t InstructionDecoder::Read(uintptr_t address)
+uint32_t InstructionDecoder::Read(uintptr_t)
 {
-    return *reinterpret_cast<uint32_t*>(address);
+    return 0;
 }
 
-bool InstructionDecoder::IsBranch(uint32_t instruction)
+bool InstructionDecoder::IsBranch(uint32_t)
 {
-    return (instruction & 0x7C000000) == 0x14000000;
+    return false;
 }
 
-bool InstructionDecoder::IsBranchLink(uint32_t instruction)
+bool InstructionDecoder::IsLoadLiteral(uint32_t)
 {
-    return (instruction & 0xFC000000) == 0x94000000;
+    return false;
 }
 
-bool InstructionDecoder::IsLoadLiteral(uint32_t instruction)
+bool InstructionDecoder::IsPcRelative(uint32_t)
 {
-    return (instruction & 0x3B000000) == 0x18000000;
+    return false;
 }
 
-bool InstructionDecoder::IsADR(uint32_t instruction)
+bool InstructionDecoder::IsBranchLink(uint32_t)
+{
+    return false;
+}
+
+bool InstructionDecoder::IsAdr(uint32_t instruction)
 {
     return (instruction & 0x9F000000) == 0x10000000;
 }
 
-bool InstructionDecoder::IsADRP(uint32_t instruction)
+bool InstructionDecoder::IsAdrp(uint32_t instruction)
 {
     return (instruction & 0x9F000000) == 0x90000000;
 }
 
-bool InstructionDecoder::IsPcRelative(uint32_t instruction)
+bool InstructionDecoder::IsB(uint32_t)
 {
-    return IsADR(instruction)
-        || IsADRP(instruction)
-        || IsLoadLiteral(instruction);
-}
-
-bool InstructionDecoder::IsB(uint32_t instruction)
-{
-    return (instruction & 0xFC000000) == 0x14000000;
+    return false;
 }
 
 }
