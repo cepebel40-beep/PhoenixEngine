501ef6c8d5327588ddd25c70c63217805e063864
cepebel40-beep <cepebel40@gmail.com>
2026-08-02 21:30:49 +0700

    Implement ARM64 instruction detection

--- src/Hook/InstructionDecoder.cpp
+++ src/Hook/InstructionDecoder.cpp
@@ -2,23 +2,47 @@
 
 namespace PHX
 {
-    uint32_t InstructionDecoder::Read(uintptr_t)
-    {
-        return 0;
-    }
 
-    bool InstructionDecoder::IsBranch(uint32_t)
-    {
-        return false;
-    }
+uint32_t InstructionDecoder::Read(uintptr_t address)
+{
+    return *reinterpret_cast<uint32_t*>(address);
+}
 
-    bool InstructionDecoder::IsLoadLiteral(uint32_t)
-    {
-        return false;
-    }
+bool InstructionDecoder::IsBranch(uint32_t instruction)
+{
+    return (instruction & 0x7C000000) == 0x14000000;
+}
 
-    bool InstructionDecoder::IsPcRelative(uint32_t)
-    {
-        return false;
-    }
+bool InstructionDecoder::IsBranchLink(uint32_t instruction)
+{
+    return (instruction & 0xFC000000) == 0x94000000;
+}
+
+bool InstructionDecoder::IsLoadLiteral(uint32_t instruction)
+{
+    return (instruction & 0x3B000000) == 0x18000000;
+}
+
+bool InstructionDecoder::IsADR(uint32_t instruction)
+{
+    return (instruction & 0x9F000000) == 0x10000000;
+}
+
+bool InstructionDecoder::IsADRP(uint32_t instruction)
+{
+    return (instruction & 0x9F000000) == 0x90000000;
+}
+
+bool InstructionDecoder::IsPcRelative(uint32_t instruction)
+{
+    return IsADR(instruction)
+        || IsADRP(instruction)
+        || IsLoadLiteral(instruction);
+}
+
+bool InstructionDecoder::IsB(uint32_t instruction)
+{
+    return (instruction & 0xFC000000) == 0x14000000;
+}
+
 }
