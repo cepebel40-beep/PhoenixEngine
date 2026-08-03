b388722ecb4948d4b476868766add6936ceb6190
cepebel40-beep <cepebel40@gmail.com>
2026-08-03 19:12:13 +0700

    Add framebuffer and vertex array management methods

--- include/Graphics/RenderContext.hpp
+++ include/Graphics/RenderContext.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <GLES3/gl3.h>
+#include <cstdint>
 
 namespace PHX
 {
@@ -13,18 +14,54 @@ public:
 
     static void Shutdown();
 
+    static void Reset();
+
     static void SetProgram(GLuint program);
     static GLuint GetProgram();
 
     static void SetTexture(GLuint texture);
     static GLuint GetTexture();
 
-    static void Reset();
+    static void SetFramebuffer(GLuint framebuffer);
+    static GLuint GetFramebuffer();
+
+    static void SetVertexArray(GLuint vao);
+    static GLuint GetVertexArray();
+
+    static void SetViewport(GLint x,
+                            GLint y,
+                            GLsizei width,
+                            GLsizei height);
+
+    static void GetViewport(GLint& x,
+                            GLint& y,
+                            GLsizei& width,
+                            GLsizei& height);
+
+    static void BeginFrame();
+
+    static void EndFrame();
+
+    static uint64_t GetFrameCount();
+
+    static uint64_t GetDrawCallCount();
+
+    static void IncrementDrawCall();
 
 private:
 
     static GLuint sProgram;
     static GLuint sTexture;
+    static GLuint sFramebuffer;
+    static GLuint sVertexArray;
+
+    static GLint sViewportX;
+    static GLint sViewportY;
+    static GLsizei sViewportWidth;
+    static GLsizei sViewportHeight;
+
+    static uint64_t sFrameCount;
+    static uint64_t sDrawCallCount;
 };
 
 }
