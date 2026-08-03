103d47c9afbd118d4ee0954be83c5ecbd1d6050a
cepebel40-beep <cepebel40@gmail.com>
2026-08-03 23:35:38 +0700

    Support vertex and fragment shader replacement
    Added shader type detection and logging for vertex and fragment shaders.

--- src/Graphics/ShaderReplacer.cpp
+++ src/Graphics/ShaderReplacer.cpp
@@ -1,7 +1,10 @@
 #include "Graphics/ShaderReplacer.hpp"
+
 #include "Graphics/ShaderInterceptor.hpp"
 
 #include "Core/Logger.hpp"
+
+#include <GLES3/gl3.h>
 
 namespace PHX
 {
@@ -41,6 +44,36 @@ std::string ShaderReplacer::ReplaceShader(
 
     std::string source =
         ShaderInterceptor::GetSource(shader);
+
+    GLenum type =
+        ShaderInterceptor::GetType(shader);
+
+    switch (type)
+    {
+        case GL_VERTEX_SHADER:
+        {
+            Logger::Info(
+                "Vertex shader detected");
+
+            break;
+        }
+
+        case GL_FRAGMENT_SHADER:
+        {
+            Logger::Info(
+                "Fragment shader detected");
+
+            break;
+        }
+
+        default:
+        {
+            Logger::Info(
+                "Unknown shader type");
+
+            break;
+        }
+    }
 
     return Replace(source);
 }
