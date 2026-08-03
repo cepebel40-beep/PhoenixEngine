47f7220aa5752be579b2033745439ecbd25a89f9
cepebel40-beep <cepebel40@gmail.com>
2026-08-03 17:50:57 +0700

    Add ShaderInterceptor class for shader management

---
+++ include/Graphics/ShaderInterceptor.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+#include <GLES3/gl3.h>
+
+namespace PHX
+{
+
+class ShaderInterceptor
+{
+public:
+
+    static bool Initialize();
+
+    static void Shutdown();
+
+    static void OnCreateShader(
+        GLuint shader,
+        GLenum type);
+
+    static void OnShaderSource(
+        GLuint shader,
+        const std::string& source);
+
+    static void OnCompileShader(
+        GLuint shader);
+
+    static void OnAttachShader(
+        GLuint program,
+        GLuint shader);
+
+    static void OnLinkProgram(
+        GLuint program);
+
+    static bool HasSource(GLuint shader);
+
+    static std::string GetSource(GLuint shader);
+
+    static GLenum GetType(GLuint shader);
+
+private:
+
+    struct ShaderInfo
+    {
+        GLenum type;
+
+        std::string source;
+
+        bool compiled;
+    };
+
+    static std::unordered_map<
+        GLuint,
+        ShaderInfo> sShaders;
+};
+
+}
