19a17f2a17cf26c442c9f5d4d64f83ffd5d19ac7
cepebel40-beep <cepebel40@gmail.com>
2026-08-03 21:41:52 +0700

    Reformat ShaderCompiler method declarations
    Refactor method signatures for better readability.

--- include/Graphics/ShaderCompiler.hpp
+++ include/Graphics/ShaderCompiler.hpp
@@ -9,11 +9,24 @@ namespace PHX
 class ShaderCompiler
 {
 public:
-    static GLuint CompileVertex(const std::string& source);
-    static GLuint CompileFragment(const std::string& source);
+
+    static GLuint CompileVertex(
+        const std::string& source);
+
+    static GLuint CompileFragment(
+        const std::string& source);
+
+    static std::string GetLastError();
 
 private:
-    static GLuint Compile(GLenum type, const std::string& source);
+
+    static GLuint Compile(
+        GLenum type,
+        const std::string& source);
+
+private:
+
+    static std::string sLastError;
 };
 
 }
