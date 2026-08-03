583014aa65a0edf93fa01cd1634d1d42178b55a5
cepebel40-beep <cepebel40@gmail.com>
2026-08-03 17:44:02 +0700

    Prepare OpenGLHook stage 6

--- src/Hook/OpenGLHook.cpp
+++ src/Hook/OpenGLHook.cpp
@@ -11,6 +11,21 @@ namespace PHX
 
 void (*Original_glUseProgram)(GLuint) = nullptr;
 
+void (*Original_glBindTexture)(
+    GLenum,
+    GLuint) = nullptr;
+
+void (*Original_glDrawElements)(
+    GLenum,
+    GLsizei,
+    GLenum,
+    const void*) = nullptr;
+
+void (*Original_glDrawArrays)(
+    GLenum,
+    GLint,
+    GLsizei) = nullptr;
+
 static bool sFirstProgram = true;
 static GLuint sCurrentProgram = 0;
 
@@ -18,7 +33,7 @@ void Hook_glUseProgram(GLuint program)
 {
     if (sFirstProgram)
     {
-        Logger::Info("First OpenGL shader program detected");
+        Logger::Info("First OpenGL shader detected");
         sFirstProgram = false;
     }
 
@@ -28,61 +43,166 @@ void Hook_glUseProgram(GLuint program)
         RenderContext::SetProgram(program);
     }
 
-    /*
-     * Panggil OpenGL asli terlebih dahulu.
-     * Renderer nantinya juga akan memakai
-     * Original_glUseProgram agar tidak
-     * terjadi recursive hook.
-     */
     if (Original_glUseProgram)
-    {
         Original_glUseProgram(program);
-    }
 
     RenderFrame();
 }
 
+void Hook_glBindTexture(
+    GLenum target,
+    GLuint texture)
+{
+    RenderContext::SetTexture(texture);
+
+    if (Original_glBindTexture)
+        Original_glBindTexture(target, texture);
+}
+
+void Hook_glDrawElements(
+    GLenum mode,
+    GLsizei count,
+    GLenum type,
+    const void* indices)
+{
+    RenderFrame();
+
+    if (Original_glDrawElements)
+    {
+        Original_glDrawElements(
+            mode,
+            count,
+            type,
+            indices);
+    }
+}
+
+void Hook_glDrawArrays(
+    GLenum mode,
+    GLint first,
+    GLsizei count)
+{
+    RenderFrame();
+
+    if (Original_glDrawArrays)
+    {
+        Original_glDrawArrays(
+            mode,
+            first,
+            count);
+    }
+}
+
 bool InstallOpenGLHooks()
 {
-    auto target =
+    bool success = true;
+
+    auto useProgram =
         reinterpret_cast<uintptr_t>(
-            SymbolResolver::ResolveOpenGL("glUseProgram"));
+            SymbolResolver::ResolveOpenGL(
+                "glUseProgram"));
 
-    if (!target)
+    if (useProgram)
     {
-        Logger::Error("glUseProgram not found");
-        return false;
+        success &= HookManager::Install(
+            useProgram,
+            reinterpret_cast<uintptr_t>(
+                &Hook_glUseProgram),
+            reinterpret_cast<uintptr_t*>(
+                &Original_glUseProgram));
     }
 
-    if (!HookManager::Install(
-            target,
-            reinterpret_cast<uintptr_t>(&Hook_glUseProgram),
-            reinterpret_cast<uintptr_t*>(&Original_glUseProgram)))
+    auto bindTexture =
+        reinterpret_cast<uintptr_t>(
+            SymbolResolver::ResolveOpenGL(
+                "glBindTexture"));
+
+    if (bindTexture)
     {
-        Logger::Error("Failed to install glUseProgram hook");
-        return false;
+        success &= HookManager::Install(
+            bindTexture,
+            reinterpret_cast<uintptr_t>(
+                &Hook_glBindTexture),
+            reinterpret_cast<uintptr_t*>(
+                &Original_glBindTexture));
     }
 
-    Logger::Info("glUseProgram hook installed");
+    auto drawElements =
+        reinterpret_cast<uintptr_t>(
+            SymbolResolver::ResolveOpenGL(
+                "glDrawElements"));
 
-    return true;
+    if (drawElements)
+    {
+        success &= HookManager::Install(
+            drawElements,
+            reinterpret_cast<uintptr_t>(
+                &Hook_glDrawElements),
+            reinterpret_cast<uintptr_t*>(
+                &Original_glDrawElements));
+    }
+
+    auto drawArrays =
+        reinterpret_cast<uintptr_t>(
+            SymbolResolver::ResolveOpenGL(
+                "glDrawArrays"));
+
+    if (drawArrays)
+    {
+        success &= HookManager::Install(
+            drawArrays,
+            reinterpret_cast<uintptr_t>(
+                &Hook_glDrawArrays),
+            reinterpret_cast<uintptr_t*>(
+                &Original_glDrawArrays));
+    }
+
+    if (success)
+        Logger::Info("OpenGL hook stage 6 ready");
+
+    return success;
 }
 
 bool RemoveOpenGLHooks()
 {
-    auto target =
-        reinterpret_cast<uintptr_t>(
-            SymbolResolver::ResolveOpenGL("glUseProgram"));
-
-    if (!target)
-        return false;
-
     sCurrentProgram = 0;
     sFirstProgram = true;
 
-    Logger::Info("Removing glUseProgram hook");
+    auto useProgram =
+        reinterpret_cast<uintptr_t>(
+            SymbolResolver::ResolveOpenGL(
+                "glUseProgram"));
 
-    return HookManager::Remove(target);
+    if (useProgram)
+        HookManager::Remove(useProgram);
+
+    auto bindTexture =
+        reinterpret_cast<uintptr_t>(
+            SymbolResolver::ResolveOpenGL(
+                "glBindTexture"));
+
+    if (bindTexture)
+        HookManager::Remove(bindTexture);
+
+    auto drawElements =
+        reinterpret_cast<uintptr_t>(
+            SymbolResolver::ResolveOpenGL(
+                "glDrawElements"));
+
+    if (drawElements)
+        HookManager::Remove(drawElements);
+
+    auto drawArrays =
+        reinterpret_cast<uintptr_t>(
+            SymbolResolver::ResolveOpenGL(
+                "glDrawArrays"));
+
+    if (drawArrays)
+        HookManager::Remove(drawArrays);
+
+    Logger::Info("OpenGL hooks removed");
+
+    return true;
 }
 
 }
