839cf4f8dbe64b3ae2a5bbaf740168e0b5945930
cepebel40-beep <cepebel40@gmail.com>
2026-08-03 22:08:34 +0700

    Validate shader source before compilation

--- src/Graphics/ShaderCompiler.cpp
+++ src/Graphics/ShaderCompiler.cpp
@@ -1,6 +1,8 @@
 #include "Graphics/ShaderCompiler.hpp"
 
 #include <android/log.h>
+#include <algorithm>
+#include <cctype>
 
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,"Phoenix",__VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"Phoenix",__VA_ARGS__)
@@ -10,23 +12,50 @@ namespace PHX
 
 std::string ShaderCompiler::sLastError;
 
+static bool IsSourceEmpty(const std::string& source)
+{
+    return std::all_of(
+        source.begin(),
+        source.end(),
+        [](unsigned char c)
+        {
+            return std::isspace(c);
+        });
+}
+
 GLuint ShaderCompiler::Compile(
     GLenum type,
     const std::string& source)
 {
     sLastError.clear();
 
+    if (source.empty() || IsSourceEmpty(source))
+    {
+        sLastError = "Shader source is empty";
+
+        LOGE("%s", sLastError.c_str());
+
+        return 0;
+    }
+
     GLuint shader = glCreateShader(type);
 
-    if(shader == 0)
+    if (shader == 0)
     {
         sLastError = "glCreateShader failed";
+
+        LOGE("%s", sLastError.c_str());
+
         return 0;
     }
 
     const char* src = source.c_str();
 
-    glShaderSource(shader,1,&src,nullptr);
+    glShaderSource(
+        shader,
+        1,
+        &src,
+        nullptr);
 
     glCompileShader(shader);
 
@@ -37,7 +66,7 @@ GLuint ShaderCompiler::Compile(
         GL_COMPILE_STATUS,
         &success);
 
-    if(!success)
+    if (!success)
     {
         char log[2048]{};
 
@@ -49,14 +78,15 @@ GLuint ShaderCompiler::Compile(
 
         sLastError = log;
 
-        LOGE("%s",log);
+        LOGE("%s", log);
 
         glDeleteShader(shader);
 
         return 0;
     }
 
-    LOGI("Shader compiled");
+    LOGI(
+        "Shader compiled successfully");
 
     return shader;
 }
