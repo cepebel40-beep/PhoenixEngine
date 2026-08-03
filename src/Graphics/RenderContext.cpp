a8607296e68375379a1e3681d340e6fd0c0e2f15
cepebel40-beep <cepebel40@gmail.com>
2026-08-03 19:13:14 +0700

    Commit #231 Upgrade RenderContext runtime state

--- src/Graphics/RenderContext.cpp
+++ src/Graphics/RenderContext.cpp
@@ -7,11 +7,20 @@ namespace PHX
 
 GLuint RenderContext::sProgram = 0;
 GLuint RenderContext::sTexture = 0;
+GLuint RenderContext::sFramebuffer = 0;
+GLuint RenderContext::sVertexArray = 0;
+
+GLint RenderContext::sViewportX = 0;
+GLint RenderContext::sViewportY = 0;
+GLsizei RenderContext::sViewportWidth = 0;
+GLsizei RenderContext::sViewportHeight = 0;
+
+uint64_t RenderContext::sFrameCount = 0;
+uint64_t RenderContext::sDrawCallCount = 0;
 
 bool RenderContext::Initialize()
 {
-    sProgram = 0;
-    sTexture = 0;
+    Reset();
 
     Logger::Info("RenderContext initialized");
 
@@ -29,6 +38,16 @@ void RenderContext::Reset()
 {
     sProgram = 0;
     sTexture = 0;
+    sFramebuffer = 0;
+    sVertexArray = 0;
+
+    sViewportX = 0;
+    sViewportY = 0;
+    sViewportWidth = 0;
+    sViewportHeight = 0;
+
+    sFrameCount = 0;
+    sDrawCallCount = 0;
 }
 
 void RenderContext::SetProgram(GLuint program)
@@ -49,6 +68,73 @@ void RenderContext::SetTexture(GLuint texture)
 GLuint RenderContext::GetTexture()
 {
     return sTexture;
+}
+
+void RenderContext::SetFramebuffer(GLuint framebuffer)
+{
+    sFramebuffer = framebuffer;
+}
+
+GLuint RenderContext::GetFramebuffer()
+{
+    return sFramebuffer;
+}
+
+void RenderContext::SetVertexArray(GLuint vao)
+{
+    sVertexArray = vao;
+}
+
+GLuint RenderContext::GetVertexArray()
+{
+    return sVertexArray;
+}
+
+void RenderContext::SetViewport(GLint x,
+                                GLint y,
+                                GLsizei width,
+                                GLsizei height)
+{
+    sViewportX = x;
+    sViewportY = y;
+    sViewportWidth = width;
+    sViewportHeight = height;
+}
+
+void RenderContext::GetViewport(GLint& x,
+                                GLint& y,
+                                GLsizei& width,
+                                GLsizei& height)
+{
+    x = sViewportX;
+    y = sViewportY;
+    width = sViewportWidth;
+    height = sViewportHeight;
+}
+
+void RenderContext::BeginFrame()
+{
+    ++sFrameCount;
+    sDrawCallCount = 0;
+}
+
+void RenderContext::EndFrame()
+{
+}
+
+uint64_t RenderContext::GetFrameCount()
+{
+    return sFrameCount;
+}
+
+uint64_t RenderContext::GetDrawCallCount()
+{
+    return sDrawCallCount;
+}
+
+void RenderContext::IncrementDrawCall()
+{
+    ++sDrawCallCount;
 }
 
 }
