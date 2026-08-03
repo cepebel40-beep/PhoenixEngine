3f3c01df2d3ae9fc49b650b212787d3b1aaa7e0d
cepebel40-beep <cepebel40@gmail.com>
2026-08-03 17:25:03 +0700

    Prepare OpenGLHook stage 4

--- include/Hook/OpenGLHook.hpp
+++ include/Hook/OpenGLHook.hpp
@@ -9,9 +9,27 @@ bool InstallOpenGLHooks();
 bool RemoveOpenGLHooks();
 
 extern void (*Original_glUseProgram)(GLuint);
-extern void (*Original_glBindTexture)(GLenum, GLuint);
+
+extern void (*Original_glBindTexture)(
+    GLenum,
+    GLuint);
+
+extern void (*Original_glDrawElements)(
+    GLenum,
+    GLsizei,
+    GLenum,
+    const void*);
 
 void Hook_glUseProgram(GLuint program);
-void Hook_glBindTexture(GLenum target, GLuint texture);
+
+void Hook_glBindTexture(
+    GLenum target,
+    GLuint texture);
+
+void Hook_glDrawElements(
+    GLenum mode,
+    GLsizei count,
+    GLenum type,
+    const void* indices);
 
 }
